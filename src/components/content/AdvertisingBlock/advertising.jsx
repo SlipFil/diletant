35cf0cba08066b852c6ec900c3a92f5ad0eba55f
SlipFIl <soliderslipfil@gmail.com>
2020-04-27 16:02:39 +0300

    new changes
    add advertising block into content, correct discription block with card

---
+++ src/components/content/AdvertisingBlock/advertising.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from "./advertising.module.css";
+
+
+
+
+
+
+const Advertising = (props) => {
+    
+  return (
+    <div className={s.advertising} >
+        <a> <img /> advertising</a>
+    </div>
+  );
+};
+export default Advertising;
--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import s from "./content.module.css";
 import Card from "./Card/Card";
+import Advertising from "./AdvertisingBlock/advertising";
 
 const Content = (props) => {
-    let cards = props.cards.map((card) => (
+    let cards = props.cards.map((card) => {if (card.id < 4) {return(
     <Card key={card.id} 
         when={card.when}
         where={card.where}
@@ -12,10 +13,21 @@ const Content = (props) => {
         name={card.name}
         discription={card.discription}
         size={card.size}
-        imgURL={card.imgURL} />));
+        imgURL={card.imgURL} />)}});
+        let cardsAfterAdventising = props.cards.map((card) => {if (card.id > 3) {return(
+          <Card key={card.id} 
+              when={card.when}
+              where={card.where}
+              what={card.what}
+              name={card.name}
+              discription={card.discription}
+              size={card.size}
+              imgURL={card.imgURL} />)}});
   return (
     <div className={s.content}>
       {cards}
+      <Advertising />
+      {cardsAfterAdventising}
     </div>
   );
 };
